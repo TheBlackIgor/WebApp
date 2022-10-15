bfd14d4def8f365e74a59b490bd88ff6fb172ab2
TheBlackIgor <swierczynskigor@gmail.com>
2022-10-15 21:03:56 +0200

    collections and functions

--- src/L003/Zad007.java
+++ src/L003/Zad007.java
@@ -4,7 +4,8 @@ import java.math.BigInteger;
 
 public class Zad007 {
     public static void main(String[] args){
-        System.out.println("10:10".hashCode());
+        Zegar z1 = new Zegar();
+        Zegar z2 = new Zegar();
     }
 }
 
